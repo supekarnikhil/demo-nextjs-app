765f5374f1334719ec0e7bae3dc60e76a28b467a
Nikhil Supekar <11524291+supekarnikhil@users.noreply.github.com>
2023-05-29 10:00:55 +0530

    feat: add tailwindui components

---
+++ src/pages/shop/[slug].tsx
@@ -0,0 +1,17 @@
+import CtaSection from "@/components/CtaSection";
+import HeroSection from "@/components/HeroSection";
+import PricingSections from "@/components/PricingSections";
+import StatSection from "@/components/StatSection";
+import TestimonialSection from "@/components/TestimonialSection";
+
+export default function Demo() {
+    return (
+        <>
+            <HeroSection />
+            <StatSection />
+            <CtaSection />
+            <PricingSections />
+            <TestimonialSection />
+        </>
+    );
+}
