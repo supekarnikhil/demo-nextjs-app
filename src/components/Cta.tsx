bba69f4c94f9a449ec9978dfb9bf7ceeeaf6443d
Nikhil Supekar <11524291+supekarnikhil@users.noreply.github.com>
2023-05-30 21:50:55 +0530

    feat: dark mode CTA, integrate Strapi in CtaSection

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -3,13 +3,41 @@ enum CtaTypes {
     "secondary"="secondary",
 }
 
+enum CtaModes {
+  "light"="light",
+  "dark"="dark",
+}
+
 type CtaProps = {
   name: string;
   link: string;
   type: CtaTypes;
+  mode: CtaModes;
 };
+
 export default function Cta(props: CtaProps) {
-  if (props.type === CtaTypes.primary) {
+
+  if (props.mode === CtaModes.dark && props.type === CtaTypes.primary) {
+    return (
+      <a
+        href={props.link}
+        className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+      >
+        {props.name}
+      </a>
+    );
+  }
+  else if (props.mode === CtaModes.dark && props.type === CtaTypes.secondary) {
+    return(
+      <a
+        href={props.link}
+        className="text-sm font-semibold leading-6 text-white"
+      >
+        {props.name} <span aria-hidden="true">→</span>
+      </a>
+    );
+  }
+  else if (props.type === CtaTypes.primary) {
     return (
       <a
         href={props.link}
--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -1,4 +1,6 @@
-export default function CtaSection() {
+import Cta from "./Cta"
+
+export default function CtaSection(props:any) {
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
@@ -18,29 +20,21 @@ export default function CtaSection() {
             </svg>
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
-                <br />
-                Start using our app today.
+                {props.title}
               </h2>
               <p className="mt-6 text-lg leading-8 text-gray-300">
-                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+                {props.shortDescription}
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <a
-                  href="#"
-                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
+                {props.CTAGroup.map((cta:any)=>{
+                  return(<Cta key={cta.id} {...cta} />)
+                  })}
               </div>
             </div>
             <div className="relative mt-16 h-80 lg:mt-8">
               <img
                 className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                src={`${process.env.CMS_BASE_URL}${props.image.data.attributes.url}`}
                 alt="App screenshot"
                 width={1824}
                 height={1080}
