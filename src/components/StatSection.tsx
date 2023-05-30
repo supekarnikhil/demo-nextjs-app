c48cf88a2d6e23b9030b30481a8403487c40c832
Nikhil Supekar <11524291+supekarnikhil@users.noreply.github.com>
2023-05-30 08:28:30 +0530

    feat: integrate strapi in Pricing, Stats, Testimonial sections

--- src/components/StatSection.tsx
+++ src/components/StatSection.tsx
@@ -1,19 +1,13 @@
-const stats = [
-    { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
-    { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-    { id: 3, name: 'New users annually', value: '46,000' },
-  ]
-  
-  export default function StatSection() {
+export default function StatSection(props:any) {
     return (
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
-            {stats.map((stat) => (
+            {props.statsList.map((stat:any) => (
               <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-                <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
+                <dt className="text-base leading-7 text-gray-600">{stat.subTitle}</dt>
                 <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                  {stat.value}
+                  {stat.title}
                 </dd>
               </div>
             ))}
